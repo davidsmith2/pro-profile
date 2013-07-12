56b53a0bece0e1c2bea42a520ed584a95dc806f4
David Smith <davidsmith2@gmail.com>
2013-07-12 17:44:02 -0400

    made subscriber functions public

--- app/js/app.js
+++ app/js/app.js
@@ -12,22 +12,30 @@ function (ApiManager, Connections, Router, AppView, AuthView, HomeView, Connecti
 
     var App = function () {
 
-        var self = this;
-
         this.apiManager = new ApiManager();
         this.views.app = new AppView();
         this.views.auth = new AuthView({ apiManager: this.apiManager });
         this.views.home = new HomeView();
-        self.collections.connections = new Connections();
-        this.views.connections = new ConnectionsView({ collection: self.collections.connections });
+        this.collections.connections = new Connections();
+        this.views.connections = new ConnectionsView({ collection: this.collections.connections });
         this.router = new Router();
 
-        this.apiManager.on('ready', onApiReady, this);
-        this.apiManager.on('authorize', onApiAuthorize, this);
-        this.apiManager.on('logout', onApiLogout, this);
+        this.apiManager.on('ready', this.init, this);
+        this.apiManager.on('authorize', this.authorize, this);
+        this.apiManager.on('logout', this.logout, this);
 
-        function onApiReady () {
-            self.views.auth.render(function () {
+    };
+
+    App.prototype = {
+
+        collections: {},
+        models: {},
+        router: {},
+        views: {},
+
+        init: function () {
+            var self = this;
+            this.views.auth.render(function () {
                 self.views.auth.$loginButton = $(self.views.auth.loginButton);
                 self.views.auth.$logoutButton = $(self.views.auth.logoutButton);
                 if (self.apiManager.isAuthorized()) {
@@ -38,25 +46,18 @@ function (ApiManager, Connections, Router, AppView, AuthView, HomeView, Connecti
                     self.views.auth.$logoutButton.hide();
                 }
             });
+        },
+
+        authorize: function () {
+            this.collections.connections.update();
+            this.views.home.$el.empty();
+        },
+
+        logout: function () {
+            this.views.connections.$el.empty();
+            this.views.home.render();
         }
 
-        function onApiAuthorize () {
-            self.collections.connections.update();
-            self.views.home.$el.empty();
-        }
-
-        function onApiLogout () {
-            self.views.connections.$el.empty();
-            self.views.home.render();
-        }
-
-    };
-
-    App.prototype = {
-        collections: {},
-        models: {},
-        router: {},
-        views: {},
     };
 
     return App;
