3a38c4e8b7d5f51dbef4bd2eeb888641c7947e82
David Smith <davidsmith2@gmail.com>
2013-07-20 01:41:00 -0400

    reverted to http

--- server.js
+++ server.js
@@ -1,30 +1,14 @@
-var express,
-    https,
-    fs,
-    options,
+var connect = require('connect'),
+    http = require('http'),
     app;
 
-// define dependencies
-express = require('express');
-https = require('https');
-fs = require('fs');
+app = connect()
+    .use(connect.static('app'))
+    .use('/js/lib/', connect.static('node_modules/requirejs/'))
+    .use('/node_modules', connect.static('node_modules'))
+    .use('/test', connect.static('test/'))
+    .use('/test', connect.static('app'));
 
-// ssl cert info
-options = {
-    key: fs.readFileSync('key.pem'),
-    cert: fs.readFileSync('cert.pem')
-};
-
-// create a new expressjs app
-app = express();
-
-app
-    .use(express.static('app'))
-    .use('/js/lib/', express.static('node_modules/requirejs/'))
-    .use('/node_modules', express.static('node_modules'))
-    .use('/test', express.static('test/'))
-    .use('/test', express.static('app'));
-
-https.createServer(options, app).listen(8080, function () {
-	console.log('Running on https://localhost:8080');
+http.createServer(app).listen(8080, function () {
+    console.log('Running on http://localhost:8080');
 });
