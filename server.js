4824b39610e9e359aba927327a61382705e1ca9b
David Smith <davidsmith2@gmail.com>
2013-07-21 22:21:58 -0400

    clean up server.js

--- server.js
+++ server.js
@@ -1,23 +1,24 @@
-var connect = require('connect'),
-    fs = require('fs'),
+var express = require('express'),
     https = require('https'),
-    app;
+    fs = require('fs');
 
 var protocol = 'https',
     port = 8443;
 
-var options = {
+var credentials = {
     key: fs.readFileSync('ssl/key.pem'),
     cert: fs.readFileSync('ssl/cert.pem')
 };
 
-app = connect()
-    .use(connect.static('app'))
-    .use('/js/lib/', connect.static('node_modules/requirejs/'))
-    .use('/node_modules', connect.static('node_modules'))
-    .use('/test', connect.static('test/'))
-    .use('/test', connect.static('app'));
+var app = express();
 
-https.createServer(options, app).listen(port, function () {
+app
+    .use(express.static('app'))
+    .use('/js/lib/', express.static('node_modules/requirejs/'))
+    .use('/node_modules', express.static('node_modules'))
+    .use('/test', express.static('test/'))
+    .use('/test', express.static('app'));
+
+https.createServer(credentials, app).listen(port, function () {
     console.log('Running on ' + protocol + '//localhost:' + port);
 });
